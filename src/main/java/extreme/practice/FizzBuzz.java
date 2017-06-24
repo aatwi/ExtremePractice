efa371effbb9c93e7bba6ed0a2aedc3bdfde3318
Ahmad Atwi <ahmdatwi23@gmail.com>
2017-06-24 15:47:11 +0200

    Prepare a branch for the SPA conference

--- src/main/java/extreme/practice/FizzBuzz.java
+++ src/main/java/extreme/practice/FizzBuzz.java
@@ -1,8 +1,5 @@
 package extreme.practice;
 
 class FizzBuzz {
-    public static int of(int i) {
-        return 1;
 
-    }
 }
--- src/test/java/extreme/practice/FizzBuzzTest.java
+++ src/test/java/extreme/practice/FizzBuzzTest.java
@@ -1,16 +1,4 @@
 package extreme.practice;
 
-import org.fest.assertions.api.Assertions;
-import org.junit.Test;
-
 public class FizzBuzzTest {
-
-
-
-
-    @Test
-    public void
-    it_should_print_1_when_given_1() {
-        Assertions.assertThat(FizzBuzz.of(1)).isEqualTo(1);
-    }
 }
